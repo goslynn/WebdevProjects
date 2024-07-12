711e40aa848b7e43cee0b9e511ad92b9d6f46e4f
goslynn <vct.gonzaleza@gmail.com>
2024-07-12 15:14:22 -0400

    Commit antes de arrglar, codigo roto 12/7/24 3:15
     Changes to be committed:
    	modified:   login/admin.html
    	modified:   login/css/admin_style.css
    	modified:   login/css/login_style.css
    	modified:   login/css/register_style.css
    	new file:   login/css/styleguide.css
    	modified:   login/home.html
    	new file:   login/img/edit.svg
    	new file:   login/img/info.svg
    	new file:   login/img/trash.svg
    	new file:   login/img/view.svg
    	new file:   login/img/view_hide.svg
    	modified:   login/js/admin.js
    	modified:   login/login.html

--- login/js/admin.js
+++ login/js/admin.js
@@ -1,4 +1,4 @@
-Users = JSON.parse(localStorage.getItem('users')) || [];
+const Users = JSON.parse(localStorage.getItem('users')) || [];
 
 //Ordenando listado por id
 Users.sort((a,b) => a.id - b.id);
@@ -14,25 +14,48 @@ for (let user of Users){
             </tr>
             <tr>
                 <th class="table_element_key">Email</th>
-                <td class="table_element_value">${user.email}</td>
+                <td class="table_element_value">
+                    <form id="editable_user_data_form">
+                        <input class="editable_user_data_input" value="${user.email}"  id="${user.id}" name="email" disabled >
+                    </form>
+                </td>
             </tr>
             <tr>
                 <th class="table_element_key">Usuario</th>
-                <td class="table_element_value">${user.username}</td>
+                <td class="table_element_value">
+                    <form id="editable_user_data_form">
+                        <input class="editable_user_data_input" value="${user.username}" id="${user.id}" name="username" disabled >
+                    </form>
+                </td>
             </tr>
             <tr>
                 <th class="table_element_key">Contraseña</th>
-                <td class="table_element_value">${user.password}</td>
+                <td class="table_element_value">
+                    <form id="editable_user_data_form">
+                        <input class="editable_user_data_input" value="${user.password}" id="${user.id}" name="password" disabled >
+                    </form>
+                </td>
             </tr>
         </table>
-        <div class="dropdown">
-            <button class="dropbtn">Actions</button>
-            <div class="dropdown-content">
-                <button id="edit" value=${user.id} >Editar</button>
-                <button id="delete" value=${user.id} >Eliminar</button>
-            </div>
+        <div class="buttons_set">
+            <button id="info" value=${user.id}>
+                <div class="icon_container">
+                    <img class="icon" src="img/info.svg" />
+                </div>
+            </button>
+            <button id="edit" value=${user.id}>
+                <div class="icon_container">
+                    <img class="icon" src="img/edit.svg" />
+                </div>
+            </button>
+            <button id="delete" value=${user.id}>
+                <div class="icon_container">
+                    <img class="icon" src="img/trash.svg" />
+                </div>
+            </button>
         </div>
     </li>
+    <button type="submit" form="editable_user_data_form" id="submit_new_user_data" value=${user.id}>CONFIRMAR</button>
     `;
     listado.insertAdjacentHTML('beforeend', userHtml);
 }
@@ -40,38 +63,84 @@ for (let user of Users){
 // nose para que servira esto
 const singleUser = document.querySelectorAll('.list_singleuser');
 
-//dropdown
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdowns = document.querySelectorAll('.dropdown');
 
-    dropdowns.forEach(dropdown => {
-        const dropbtn = dropdown.querySelector('.dropbtn');
-        const dropdownContent = dropdown.querySelector('.dropdown-content');
+// Eliminar
+const deleteUserPopup = document.querySelector('#delete_user_popup');
+const deleteUsereCancel = document.querySelector('#cancel')
+const deleteUserConfirm = document.querySelector('#confirm')
+const deleteButtons = document.querySelectorAll('#delete');
 
-        dropbtn.addEventListener('click', () => {
-            dropdownContent.classList.toggle('show');
-        });
+// Note to self: No nestear eventListeners pq queda la zorra
 
-        window.addEventListener('click', (event) => {
-            if (!event.target.matches('.dropbtn')) {
-                if (dropdownContent.classList.contains('show')) {
-                    dropdownContent.classList.remove('show');
-                }
+deleteUsereCancel.addEventListener('click', () => {
+    deleteUserPopup.close();
+});
+
+deleteUserConfirm.addEventListener('click', () => {
+    deleteUserPopup.close();
+    let adminPassword = window.prompt('password');
+    if (adminPassword !== 'admin') {
+        window.alert('Contraseña incorrecta');
+        return;
+    }
+    const thisUserId = parseInt(deleteUserConfirm.dataset.userId);
+    if (thisUserId === 0){
+        window.alert('No puedes eliminar al admin');
+        return;
+    }
+    Users = Users.filter(user => user.id !== thisUserId);
+    localStorage.setItem('users', JSON.stringify(Users));
+    window.location.reload();
+});
+
+deleteButtons.forEach(button =>{
+    button.addEventListener('click', () => {
+        deleteUserPopup.showModal();
+        deleteUserConfirm.dataset.userId = button.value; // Guardar el ID del usuario en un atributo de datos
+    });
+});
+
+
+//Editarz
+
+const editButtons = document.querySelectorAll('#edit');
+const editableUserData = document.querySelectorAll('.editable_user_data_input');
+const submitNewUserData = document.querySelector('#submit_new_user_data');
+
+submitNewUserData.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const inputedEmail = document.querySelector('.editable_user_data_input[name="email"]').value;
+    const inputedUsername = document.querySelector('.editable_user_data_input[name="username"]').value;
+    const inputedPassword = document.querySelector('.editable_user_data_input[name="password"]').value;
+    
+    const userId = submitNewUserData.value;
+    
+    let user = Users.find(storedUser => storedUser.id === userId);
+    
+    if (user) {
+        user.email = inputedEmail;
+        user.username = inputedUsername;
+        user.password = inputedPassword;
+        
+        Users = Users.map(storedUser => storedUser.id === userId ? user : storedUser);
+        localStorage.setItem('users', JSON.stringify(Users));
+    } else {
+        console.log('User not found');
+    }
+});
+
+editButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.value)
+        const userId = parseInt(button.value);
+        const user = Users.find(user => user.id === userId);
+        const thisEditableUserData = document.querySelectorAll(`.editable_user_data_input[id="${userId}"]`);
+        thisEditableUserData.forEach(input => {
+            if (input.disabled){
+                input.disabled = false;
+                input.focus();  
             }
         });
     });
 });
-
-// Eliminar
-
-const deleteButtons = document.querySelectorAll('#delete');
-for (let button of deleteButtons){
-    button.addEventListener('click', () => {
-        const thisUserId = parseInt(button.value);
-        Users = Users.filter(user => user.id !== thisUserId);
-        localStorage.setItem('users', JSON.stringify(Users));
-        window.location.reload();
-    })
-}
-
-    
