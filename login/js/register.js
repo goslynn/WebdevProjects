6391f5c290dd8051214c024da2a62c74cd6c414e
goslynn <vct.gonzaleza@gmail.com>
2024-08-11 23:21:52 -0400

    se agrega requisitos para las contraseñas en el formulario de registro, se agrega un archivo python para futura conexión con base de datos, se agrega botón de mostrar y ocultar contraseña en el formulario de registro.
    Changes to be committed:
    	modified:   login/css/register_style.css
    	new file:   login/img/eyeclosed.svg
    	new file:   login/img/eyeopen.svg
    	modified:   login/js/register.js
    	modified:   login/login.html
    	new file:   login/python/app.py
    	modified:   login/register.html

--- login/js/register.js
+++ login/js/register.js
@@ -1,3 +1,5 @@
+
+
 const Users = JSON.parse(localStorage.getItem('users')) || [];
 
 let userIdCounter = parseInt(localStorage.getItem('userIdCounter')) || 1;
@@ -9,34 +11,67 @@ function checkEntrys(){
         password: document.querySelector('#password').value,
         confirmedPassword: document.querySelector('#confirmed_password').value
     }
-    for (key in formData){
-        if (formData[key] === "" || formData.password !== formData.confirmedPassword){
-            return false;
-        }
+    
+    // Verificar si las contraseñas coinciden
+    if (formData.password !== formData.confirmedPassword){
+        alert("Las contraseñas no coinciden");
+        return false;
     }
+
+    // Verificar si tiene al menos 1 mayuscula, 1 minuscula, 1 numero y 1 caracter especial
+    const specialCharacters = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\\", ']', '^', '_', '`', '{', '|', '}', '~']
+    let hasSpecialCharacters = specialCharacters.some(character => formData.password.includes(character)) 
+    let hasUppercase = /[A-Z]/.test(formData.password)
+    let hasLowercase = /[a-z]/.test(formData.password)
+    let hasNumber = /[0-9]/.test(formData.password)
+    
+    console.log(hasSpecialCharacters, hasUppercase, hasLowercase, hasNumber)
+    
+    if (!hasSpecialCharacters || !hasUppercase || !hasLowercase || !hasNumber) {
+        let missingRequirements = [];
+        
+        if (!hasSpecialCharacters) missingRequirements.push('1 carácter especial');
+        if (!hasUppercase) missingRequirements.push('1 letra mayúscula');
+        if (!hasLowercase) missingRequirements.push('1 letra minúscula');
+        if (!hasNumber) missingRequirements.push('1 número');
+        
+        alert(`La contraseña debe contener al menos ${missingRequirements.join(', ')}.`);
+        return false;
+    }
+    
     return formData;
 };
 
 function registerUser(user){
-    const isRegistered = Users.find(registeredUser => registeredUser.email === user.email);
+    const isRegistered = Users.find(registeredUser => registeredUser.username === user.username);
     if (isRegistered){
         return alert("Este usuario ya está registrado");
     }
+    
     // Ejecuta en orden, asigna id, incrementa en 1, guarda en localstorage
     user.id = userIdCounter;
     userIdCounter++;
     localStorage.setItem('userIdCounter', userIdCounter);
-
+    
     Users.push(user);
     localStorage.setItem('users', JSON.stringify(Users))
     alert("Usuario registrado con exito");
 }
 
+const form = document.querySelector('#register');
 const submit = document.querySelector("#submit");
 submit.addEventListener("click", function(){
     event.preventDefault();
+    
+    // Verificar si el formulario es válido según la validación de HTML
+    if (!form.checkValidity()) {
+        // Mostrar los mensajes de error del navegador
+        form.reportValidity();
+        return;
+    }
+    
+    // Verificacion personalizada
     if (!checkEntrys()){
-        alert("Por favor, llena todos los campos");
         return;
     }
     else {
@@ -50,3 +85,53 @@ submit.addEventListener("click", function(){
     }
 });
 
+// Show password
+
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordInputFields = document.querySelectorAll('input[type="password"]');
+    const showPasswordButtons = document.querySelectorAll('.showPassword');
+
+    passwordInputFields.forEach(field => {
+        // Añadir la clase 'show' cuando el campo recibe el enfoque
+        field.addEventListener("focus", function() {
+            this.nextElementSibling.classList.add('show');
+        });
+
+        // Quitar la clase 'show' cuando el campo pierde el enfoque
+        field.addEventListener("focusout", function(event) {
+            setTimeout(() => {
+                // Si el botón de mostrar contraseña está enfocado o el clic está en el botón de mostrar
+                if (!this.nextElementSibling.contains(document.activeElement)) {
+                    this.nextElementSibling.classList.remove('show');
+                }
+            }, 100);
+        });
+    });
+
+    showPasswordButtons.forEach(button => {
+        button.addEventListener("mousedown", function(event) {
+            event.preventDefault(); // Previene el evento de focusout del campo de contraseña
+            const passwordField = this.previousElementSibling;
+            if (passwordField.type === "password") {
+                passwordField.type = "text";
+                const showButton = this.querySelector('img');
+                showButton.src = "img/eyeopen.svg";
+            } else {
+                passwordField.type = "password";
+                const showButton = this.querySelector('img');
+                showButton.src = "img/eyeclosed.svg";
+            }
+        });
+    });
+
+ 
+    // Quitar la clase 'show' si se hace clic fuera de los campos de entrada y los botones
+    document.addEventListener("click", function(event) {
+        const isClickInside = Array.from(passwordInputFields).some(field => field.contains(event.target) || field.nextElementSibling.contains(event.target));
+        if (!isClickInside) {
+            showPasswordButtons.forEach(button => {
+                button.classList.remove('show');
+            });
+        }
+    });
+});
